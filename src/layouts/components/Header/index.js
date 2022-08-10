8dedf08bdbb16e6f114d3c2d3f289ddbe160f4b5
hvnganh <102368559+hvnganh@users.noreply.github.com>
2022-08-10 15:51:35 +0700

    navbarmobile

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -3,7 +3,8 @@ import styles from './Header.module.scss';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faReact } from '@fortawesome/free-brands-svg-icons';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import { faArrowCircleDown } from '@fortawesome/free-solid-svg-icons';
 import { ThemeContext } from './ThemeContext/ThemeContext';
 import Tippy from '@tippyjs/react';
 
@@ -12,17 +13,26 @@ const cx = classNames.bind(styles);
 function Header() {
     const value = useContext(ThemeContext);
     const navigate = useNavigate();
+    const [isActive, setIsActive] = useState(false);
 
     const handleHome = () => {
         navigate('/');
+        setIsActive(false);
     };
 
     const handleProjects = () => {
         navigate('/projects');
+        setIsActive(false);
     };
 
     const handleEShop = () => {
         navigate('/eshop');
+        setIsActive(false);
+    };
+
+    const handleActiveMenu = () => {
+        setIsActive(isActive === false ? true : false);
+        console.log(isActive);
     };
 
     return (
@@ -40,6 +50,41 @@ function Header() {
                 <Tippy className={cx('tippy', value.theme)} placement="right-end" content="Change Color">
                     <FontAwesomeIcon onClick={value.toggleTheme} className={cx('icon', value.theme)} icon={faReact} />
                 </Tippy>
+            </div>
+            <div className={cx('link-items', 'mobile')}>
+                <div className={cx('mobile-menu')}>
+                    <FontAwesomeIcon
+                        onClick={handleActiveMenu}
+                        className={cx('icon', value.theme)}
+                        icon={faArrowCircleDown}
+                    />
+                </div>
+                <ul className={cx('mobile-item-link', isActive ? 'active' : '')}>
+                    <li className={cx('mobile-item')}>
+                        <button onClick={handleHome} className={cx('link-btn', 'mobile-btn', value.theme)}>
+                            About
+                        </button>
+                    </li>
+                    <li className={cx('mobile-item')}>
+                        <button onClick={handleProjects} className={cx('link-btn', 'mobile-btn', value.theme)}>
+                            Projects
+                        </button>
+                    </li>
+                    <li className={cx('mobile-item')}>
+                        <button onClick={handleEShop} className={cx('link-btn', 'mobile-btn', value.theme)}>
+                            E-Shop
+                        </button>
+                    </li>
+                    <li className={cx('mobile-item')}>
+                        <Tippy className={cx('tippy', value.theme)} placement="right-end" content="Change Color">
+                            <FontAwesomeIcon
+                                onClick={value.toggleTheme}
+                                className={cx('icon', 'mobile-btn', value.theme)}
+                                icon={faReact}
+                            />
+                        </Tippy>
+                    </li>
+                </ul>
             </div>
         </div>
     );

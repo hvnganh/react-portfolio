3f19f3b313993e51cc40cfcccf0fd1abec13ad42
hvnganh <102368559+hvnganh@users.noreply.github.com>
2022-08-18 00:17:09 +0700

    update LP

--- src/LandingPage/MainPage/MainPage.js
+++ src/LandingPage/MainPage/MainPage.js
@@ -1,4 +1,7 @@
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
+import { useEffect, useState } from 'react';
 import Content from '../components/ContentLP/Content';
 import Footer from '../components/FooterLP/Footer';
 import Header from '../components/HeaderLP/Header';
@@ -7,6 +10,24 @@ import styles from './MainPage.module.scss';
 const cx = classNames.bind(styles);
 
 function MainPage() {
+    const [showGoToTop, setShowGoToTop] = useState(false);
+
+    useEffect(() => {
+        const handleGoToTop = () => {
+            setShowGoToTop(window.scrollY > 200);
+        };
+
+        window.addEventListener('scroll', handleGoToTop);
+
+        return () => {
+            window.removeEventListener('scroll', handleGoToTop);
+        };
+    }, []);
+
+    const handleClickToTop = () => {
+        window.scrollTo(0, 0);
+    };
+
     return (
         <div className={cx('wrapper')}>
             <div className={cx('header-landing-page')}>
@@ -17,6 +38,13 @@ function MainPage() {
             </div>
             <div className={cx('footer-landing-page')}>
                 <Footer />
+            </div>
+            <div className={cx('btn-wrapper')}>
+                {showGoToTop && (
+                    <button onClick={handleClickToTop} className={cx('btn')}>
+                        <FontAwesomeIcon className={cx('icon-up')} icon={faArrowUp} />
+                    </button>
+                )}
             </div>
         </div>
     );
